4c6b5f5ac0f45010b870e630a23c02a9661aee1c
Ehsan K. N. Elameen <115994928+Ehsankhalid88@users.noreply.github.com>
2023-02-09 05:26:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+//function that multiples two numbers
+function multiply(a, b){
+   var c = a * b;
+   console.log( "The result is" , c); 
+  }
+  multiply(5,3);
+ 
+  //variables for my first name, last name, Stutern track, 
+  //skills, favourite color, year of your country’s independence
+
+  let myfirstname = 'Ehsan';
+  let lastname = ' Elameen';
+  let stuternTrack = 'Mobile Development';
+  let skills = 'Problem-Solving, Flexibility and Creativity';
+  let color = 'red ';
+  let independence = '1959';
+   var sentence = "My Name is " + myfirstname + lastname + ". My Stutern Track is " 
+   + stuternTrack + ". I have some skills such as " + skills + ". I love the " + color +
+    "color." + "The independence of my country Sudan was in " + independence + "."
+// log the sentence in the console.
+  console.log( sentence);
+  //OR we can write the sentencs directly in the console as:
+  // console.log( "My Name is " + myfirstname + lastname + ". My Stutern Track is " + stuternTrack + ". I have some skills such as " + skills + ". I love the " + color + "color." + "The independence of my country Sudan was in " + independence + ".")
